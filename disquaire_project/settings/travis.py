453c20570d3cd0cf0eb3178e908887a2d0151b7e
orion <lu@exemple.com>
2020-05-11 14:16:42 +0000

    travis.py

---
+++ disquaire_project/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
