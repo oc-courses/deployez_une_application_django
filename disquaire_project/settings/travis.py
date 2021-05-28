501f6c09bd9207ca9beae784e11bec8ef21eab64
seria <seria.medard.pge2018@gmail.com>
2021-05-28 12:26:56 +0000

    update travis5

---
+++ disquaire_project/settings/travis.py
@@ -0,0 +1,11 @@
+from . import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
